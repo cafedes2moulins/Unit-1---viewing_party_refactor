205a3b27485700feb19da5c6b75aca26b2b121ec
Tara <talsaidi@ucdavis.edu>
2022-09-30 12:01:12 -0700

    defined classes Movie and Person and built some tests for both

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,9 +1,23 @@
 import pytest
+from viewing_party.movie import Movie
 
 def test_1():
     # Arrange
+    #(title, genre, rating, host)
+    # Act
+    spider_man = Movie("Spider Man", "Action", 2, "Netflix")
+    # Assert
+    assert(spider_man.title == "Spider Man")
+    assert(spider_man.genre == "Action")
+    assert(spider_man.rating == 2)
+    assert(spider_man.host == "Netflix")
+
+def test_update_rating():
+    # Arrange
+    spider_man = Movie("Spider Man", "Action", 2, "Netflix")
 
     # Act
+    spider_man.update_rating(5)
 
     # Assert
-    pass
+    assert(spider_man.rating == 5)
--- tests/test_person.py
+++ tests/test_person.py
@@ -1,9 +1,14 @@
 import pytest
+from viewing_party.person import Person
+
 
 def test_1():
-    # Arrange
-
-    # Act
+    # Arrange and Act
+    ariel_movie_list = Person("Ariel", [], ["Julia", "Tara"], ["Netflix", "Hulu"])
+   
 
     # Assert
-    pass
+    assert(ariel_movie_list.name == "Ariel")
+    assert(ariel_movie_list.watched == [])
+    assert(ariel_movie_list.friends == ["Julia", "Tara"])
+    assert(ariel_movie_list.subscriptions == ["Netflix", "Hulu"])
